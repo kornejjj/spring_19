2f1dfa2d3b4dc95fb4409487d79bd5d511ef10b6
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-12-20 18:28:36 +0100

    Initial commit

---
+++ src/test/java/com/example/githubactionsdemo/SampleTest.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SampleTest {
+
+    @Test
+    void testSample() {
+        assertTrue(true);
+    }
+}
+
